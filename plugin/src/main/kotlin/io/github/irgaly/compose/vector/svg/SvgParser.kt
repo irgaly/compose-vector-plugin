a0b776dd933bbfce4f8e7b84a43ad7e3d0bb42f1
irgaly <irgaly@gmail.com>
2024-06-24 22:48:13 +0900

    Parse SVG width, height, viewBox

--- plugin/src/main/kotlin/io/github/irgaly/compose/vector/svg/SvgParser.kt
+++ plugin/src/main/kotlin/io/github/irgaly/compose/vector/svg/SvgParser.kt
@@ -2,6 +2,9 @@ package io.github.irgaly.compose.vector.svg
 
 import io.github.irgaly.compose.vector.node.ImageVector
 import org.apache.batik.anim.dom.SAXSVGDocumentFactory
+import org.apache.batik.anim.dom.SVGOMAnimatedLength
+import org.apache.batik.anim.dom.SVGOMAnimatedRect
+import org.apache.batik.anim.dom.SVGOMDocument
 import org.apache.batik.util.XMLResourceDescriptor
 import java.io.IOException
 import java.io.InputStream
@@ -12,32 +15,43 @@ import java.io.InputStream
 class SvgParser {
     /**
      * @throws IOException
+     * @throws IllegalStateException parse error
      */
     fun parse(input: InputStream): ImageVector {
         val doc = try {
             SAXSVGDocumentFactory(
                 XMLResourceDescriptor.getXMLParserClassName()
-            ).createDocument("xml", input)
+            ).createDocument("xml", input) as SVGOMDocument
         } catch (error: IOException) {
             throw error
         }
-        val nodes = mutableListOf<ImageVector.VectorNode>()
-        nodes.add(
+        val root = doc.rootElement
+        val viewBox = (root.viewBox as SVGOMAnimatedRect)
+        val viewBoxX = if (viewBox.isSpecified) root.viewBox.baseVal.x else 0f
+        val viewBoxY = if (viewBox.isSpecified) root.viewBox.baseVal.y else 0f
+        var viewBoxWidth = if (viewBox.isSpecified) root.viewBox.baseVal.width else null
+        var viewBoxHeight = if (viewBox.isSpecified) root.viewBox.baseVal.height else null
+        val width = if ((root.width as SVGOMAnimatedLength).isSpecified) {
+            root.width.baseVal.valueInSpecifiedUnits
+        } else viewBoxWidth ?: error("svg tag has no width")
+        val height = if ((root.height as SVGOMAnimatedLength).isSpecified) {
+            root.height.baseVal.valueInSpecifiedUnits
+        } else viewBoxHeight ?: error("svg tag has no width")
+        if (viewBoxWidth == null) {
+            viewBoxWidth = width
+        }
+        if (viewBoxHeight == null) {
+            viewBoxHeight = height
+        }
+        // TODO: svg fill attributeに対応する
+        // TODO: useタグに対応する
+        // * id付きのタグをfunで定義して使い回す
+        var rootNodes = mutableListOf<ImageVector.VectorNode>()
+        rootNodes.add(
             ImageVector.VectorNode.VectorGroup(
-                "group1",
-                0f,
-                0f,
-                0f,
-                0f,
-                0f,
-                0f,
-                0f,
-                listOf(
-                    ImageVector.PathNode.HorizontalTo(10f),
-                    ImageVector.PathNode.VerticalTo(99f)
-                ),
                 listOf(
                     ImageVector.VectorNode.VectorGroup(
+                        emptyList(),
                         "group2",
                         0f,
                         0f,
@@ -49,8 +63,7 @@ class SvgParser {
                         listOf(
                             ImageVector.PathNode.HorizontalTo(10f),
                             ImageVector.PathNode.VerticalTo(99f)
-                        ),
-                        emptyList()
+                        )
                     ),
                     ImageVector.VectorNode.VectorPath(
                         listOf(
@@ -71,10 +84,22 @@ class SvgParser {
                         0f,
                         0f
                     )
-                )
+                ),
+                "group1",
+                0f,
+                0f,
+                0f,
+                0f,
+                0f,
+                0f,
+                0f,
+                listOf(
+                    ImageVector.PathNode.HorizontalTo(10f),
+                    ImageVector.PathNode.VerticalTo(99f)
+                ),
             )
         )
-        nodes.add(
+        rootNodes.add(
             ImageVector.VectorNode.VectorPath(
                 listOf(
                     ImageVector.PathNode.HorizontalTo(10f),
@@ -95,14 +120,25 @@ class SvgParser {
                 0f
             )
         )
+        if (viewBoxX != 0f || viewBoxY != 0f) {
+            // translate by viewBox
+            rootNodes = mutableListOf(
+                ImageVector.VectorNode.VectorGroup(
+                    nodes = rootNodes,
+                    translationX = if (viewBoxX != 0f) -viewBoxX else null,
+                    translationY = if (viewBoxY != 0f) -viewBoxY else null,
+                    clipPathData = emptyList(),
+                )
+            )
+        }
         val imageVector = ImageVector(
             name = "IconName",
-            defaultWidth = 100.0,
-            defaultHeight = 100.0,
-            viewportWidth = 100f,
-            viewportHeight = 100f,
+            defaultWidth = width.toDouble(),
+            defaultHeight = height.toDouble(),
+            viewportWidth = viewBoxWidth,
+            viewportHeight = viewBoxHeight,
             autoMirror = false,
-            nodes = nodes.toList()
+            nodes = rootNodes.toList()
         )
         return imageVector
     }
